1f18652132708c991577ca9ab68a875e34c6a8ef
David Sanders <dsanders@rapilabs.com>
2017-05-01 19:20:29 +1000

    Added sesh's html5 boilerplate

--- backend/django_shopfront/urls.py
+++ backend/django_shopfront/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.static import serve
 
 urlpatterns = [
+    url('^(?:index\.html)?$', ensure_csrf_cookie(serve), {'path': 'index.html', 'document_root': settings.STATIC_ROOT}),
     url(r'^admin/', admin.site.urls),
 ]
