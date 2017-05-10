afa28dddf38ea54a7ba971d9cabcc788152845e8
David Sanders <dsanders@rapilabs.com>
2017-05-10 16:15:00 +1000

    Split up webpack config as the HMR still seems to try to connect to a dev server in production?

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,8 @@ const extractSass = new ExtractTextPlugin({
 
 const baseDir = __dirname + '/frontend/';
 
-module.exports = {
+const config = {
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
     baseDir + 'src/main.jsx',
   ],
   output: {
@@ -39,18 +36,35 @@ module.exports = {
     extensions: ['.js', '.jsx'],
     modules: [baseDir + 'src', 'node_modules'],
   },
-  devServer: {
+  plugins: [
+    extractSass,
+  ],
+};
+
+if (process.env.NODE_ENV === 'development') {
+  config.entry = [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    baseDir + 'src/main.jsx',
+  ];
+
+  config.devServer = {
     contentBase: baseDir + 'dist',
     hot: true,
     publicPath: '/js/',
     proxy: {
       '/api': 'http://127.0.0.1:8000',
     },
-  },
-  devtool: 'inline-source-map',
-  plugins: [
+  };
+
+  config.devtool = 'inline-source-map';
+
+  config.plugins = [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     extractSass,
-  ],
-};
+  ];
+}
+
+module.exports = config;
